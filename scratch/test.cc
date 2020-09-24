e3569be9044302b062e9ebc087d3673447d68c0a
Mattia Lecci <mattia.lecci@gmail.com>
2020-09-24 13:31:22 +0200

    Add scratch for bf cb testing

---
+++ scratch/test.cc
@@ -0,0 +1,42 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/file-beamforming-codebook.h"
+#include "ns3/beamforming-codebook.h"
+#include "ns3/uniform-planar-array.h"
+#include "ns3/string.h"
+#include "ns3/pointer.h"
+#include "ns3/uinteger.h"
+
+using namespace ns3;
+using namespace mmwave;
+
+NS_LOG_COMPONENT_DEFINE ("CodebookScratch");
+
+int 
+main (int argc, char *argv[])
+{
+  NS_LOG_UNCOND ("Testing bf codebook");
+
+  Ptr<PhasedArrayModel> ant = CreateObjectWithAttributes<UniformPlanarArray> ("NumRows", UintegerValue (8),
+                                                                              "NumColumns", UintegerValue (8));
+  Ptr<BeamformingCodebook> cb = CreateObjectWithAttributes<FileBeamformingCodebook> ("Array", PointerValue (ant),
+                                "CodebookFilename", StringValue ("src/mmwave/model/Codebooks/8x8.txt")
+                                );
+  cb->Initialize ();
+  NS_LOG_UNCOND ("cb size: " << cb->GetCodebookSize ());
+}
